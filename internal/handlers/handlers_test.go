6ff417f387ac7d8a0f7b515fc00fedb65cf80d6f
igolubev <igolubevapp@gmail.com>
2022-04-18 15:09:16 +0300

    Crate handlers test

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+type postData struct {
+	key 	string
+	value string
+}
+
+var theTest = []struct {
+	name 								string
+	url  								string
+	method 							string
+	params 							[]postData
+	expectedStatusCode 	int 
+}{
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"gq", "/rooms/general-quaters", "GET", []postData{}, http.StatusOK},
+	{"ms", "/rooms/majors-suite", "GET", []postData{}, http.StatusOK},
+	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
+	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"post-search-avail", "/search-availability", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-05"},
+	}, http.StatusOK},
+	{"post-search-avail-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-05"},
+	}, http.StatusOK},
+	{"make reservation post", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "John"},
+		{key: "last_name", value: "Smith"},
+		{key: "email", value: "js@mail.com"},
+		{key: "phone", value: "555-555-5555"},
+	}, http.StatusOK},
+}
+
+func TestHandlers(t *testing.T) {
+	routes := getRoutes()
+	
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTest {
+		if e.method == "GET" {
+			response, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if response.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, response.StatusCode)
+			}
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+
+			response, err := ts.Client().PostForm(ts.URL + e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if response.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, response.StatusCode)
+			}
+		}
+	}
+}
